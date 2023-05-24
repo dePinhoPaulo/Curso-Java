dccf4a0e9186e2b06dbbd21e8311aba2841ac800
Paulo Rogerio de Pinho <101976615+dePinhoPaulo@users.noreply.github.com>
2023-05-24 16:14:54 -0300

    Add files via upload

---
+++ Heranca,Polimorfismo/Heranca/src/heranca/Heranca.java
@@ -0,0 +1,14 @@
+
+package heranca;
+
+import entities.BusinessAcount;
+
+public class Heranca {
+
+    public static void main(String[] args) {
+        BusinessAcount account = new BusinessAcount();
+        
+        account.getBalance();
+    }
+    
+}
